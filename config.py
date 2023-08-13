6d8624f7aa4b8c4f4a0c0e7ddea951ddc17be053
baichuanzhou <baichuanzhou44@gmail.com>
2023-08-14 03:26:23 +0800

    add gpt2-large and extremely large

--- config.py
+++ config.py
@@ -9,12 +9,15 @@ PREFIX_MAP = {
 class LinearMappingConfig:
     image_model: str = "openai/clip-vit-base-patch32"
     freeze_image_model: bool = True
-    text_model: str = "gpt2"
+    text_model: str = "gpt2-large"
     freeze_text_model: bool = True
     image_hidden_size: int = 768
-    text_hidden_size: int = 768
+    text_hidden_size: int = 1280
     linear_mapping_type: int = "linear"
     max_seq_length: int = 2048
+    image_resize: int = 224
+    add_image_token: bool = True
+    freeze_ln: bool = False
 
     def __post_init__(self):
         self.prefix_length = PREFIX_MAP[self.image_model]
